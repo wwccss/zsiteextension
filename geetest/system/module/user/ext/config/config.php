1b2e08e6677cd5f3ae0ede8fd092857eeeb76253
wwccss <chunsheng@cnezsoft.com>
2016-09-18 15:38:37 +0800

    - remote it.

--- geetest/system/module/user/ext/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/**
-*  Geetest配置文件
-* @author Tanxu
-*/
-define("CAPTCHA_ID", "6233fdcde85059a0e0bee8cb08849ba4");
-define("PRIVATE_KEY", "4000cda10078615d9795573bba05a9a7");
-
- ?>
--- geetest/system/module/user/ext/view/login.html.php
+++
@@ -1,2 +0,0 @@
-<?php if(!defined("RUN_MODE")) die();?>
-<?php RUN_MODE == 'front' ? include 'login.front.html.php' : include 'login.admin.html.php';?>
